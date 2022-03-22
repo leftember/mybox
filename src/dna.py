5f689894313ea106cbe12137ec8137d2de603615
leftember <leftember@gmail.com>
2022-03-22 06:08:17 +0000

    bit improvement

--- src/dna.py
+++ src/dna.py
@@ -79,73 +79,26 @@ class trie:
 
     def search_text(self, text, first, last):
         score = 0
-        start = 0
         n = self.root
-        while start < len(text):
-            index = ord(text[start]) - 97
+        for cc in text:
+            index = ord(cc) - 97
             while True:
                 if n.nodes[index]:
                     n = n.nodes[index]
                     break
                 if n == self.root:
                     break
-#                print(f'moving to next {n.val}')
                 n = n.end
             # calcuate matches.
             for m in n.term:
                 #print(f'found {m.val} in gene: {m.health} --- {first}----{last}')
-                mlen = len(m.health)
                 left = bisect.bisect_left(m.health, first)
-                if left >= mlen:
+                if left >= len(m.health):
                     continue
                 right = bisect.bisect_right(m.health, last)
-                if right == 0 and m.health[right] != last:
-                    continue
                 for iii in range(left, right):
                     #print(f'adding {self.HHH[m.health[iii]]}')
                     score += self.HHH[m.health[iii]]
-#                rl = len(m.health)
-#                left = 0
-#                leftv = m.health[left][0]
-#                while rl > 1 and leftv < first:
-#                    mid = left + rl//2
-#                    midv = m.health[mid][0]
-#                    if midv > first:
-#                        rl = rl // 2
-#                    elif midv < first:
-#                        ri = rl // 2
-#                        left = mid
-#                        leftv = m.health[left][0]
-#                    else:
-#                        left = mid
-#                        break
-#                if leftv < first:
-#                    continue
-#                rl = len(m.health)
-#                right = rl-1
-#                rightv = m.health[right][0]
-#                while rl > 1 and rightv > last:
-#                    mid = right - rl // 2
-#                    midv = m.health[mid][0]
-#                    if midv > last:
-#                        rl = rl //2
-#                        right = mid
-#                        rightv = m.health[right][0]
-#                    elif midv < last:
-#                        ri = rl //2
-#                    else:
-#                        right = mid
-#                        break
-#                if rightv > last:
-#                    continue
-#                for i in range(left, right+1):
-#                    print(f'{i}-{m.health[i][1]}')
-#                    score += m.health[i][1]
-#
-                #for k,v in m.health:
-                #    if first <= k and k <= last:
-                #        score += v
-            start += 1
         return score
 
 
@@ -157,22 +110,14 @@ if __name__ == '__main__':
     low = float('inf')
     high = 0
     tree = trie()
-    #print(f'building {datetime.datetime.now().time()}')
+    print(f'building {datetime.datetime.now().time()}')
     tree.build_tree(genes, health)
+    print('tree done')
 
     for s_itr in range(s):
-        first_multiple_input = input().rstrip().split()
-
-        first = int(first_multiple_input[0])
-
-        last = int(first_multiple_input[1])
-
-        d = first_multiple_input[2]
+        first, last, d = input().split()
         #print(d)
-        score = tree.search_text(d, first, last)
-        if score > high:
-            high = score
-        if score < low:
-            low = score
+        score = tree.search_text(d, int(first), int(last))
+        low, high = min(low, score), max(high, score)
     print(f'{low} {high}')
 
